83734408b8fce2418066b33421c4656d93a99cf5
MusH <collinsmucheru@gmail.com>
2019-08-07 13:47:21 +0300

    basic http get

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## Build
 When building for production, remember to include the app/config/production.props file with `BASE_URL="host:port"` in your build environment.
+
+## Known issues
+The app uses a http endpoint which necessitates dropping our guard until the matter is resolved. Hence the line `android:usesCleartextTraffic="true"` in `AndroidManifest.xml`.
--- app/src/main/java/interview/cmush/shauri/MainActivity.java
+++ app/src/main/java/interview/cmush/shauri/MainActivity.java
@@ -3,6 +3,13 @@ package interview.cmush.shauri;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        UshauriApi ushauriApi = retrofit.create(UshauriApi.class);
+        Call<Fortune> fortuneCall = ushauriApi.getFortune();
+        fortuneCall.enqueue(new Callback<Fortune>() {
+            @Override
+            public void onResponse(Call<Fortune> call, Response<Fortune> response) {
+                if (!response.isSuccessful()) {
+                    Log.d("onResponse", String.valueOf(response.body().getFortune().get(0)));
+                    return;
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Fortune> call, Throwable t) {
+                Log.d("onResponse", t.getMessage());
+            }
+        });
     }
 }
